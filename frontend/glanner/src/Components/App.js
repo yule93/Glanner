81c85cf95a26c21b7c1bb9aabd74da92f30873f0
여율 <dbfm2222@gmail.com>
2022-01-27 16:28:40 +0900

    S06P12A606-55 feat: 사이드바 네비게이션을 Router 안으로 옮기고 링크에 따라 분기될 수 있게 구현

--- frontend/glanner/src/Components/App.js
+++ frontend/glanner/src/Components/App.js
@@ -1,4 +1,3 @@
-import Navigator from "./Navigator";
 import GlobalStyles from "./GlobalStyles";
 import styled from "styled-components";
 import Router from "../Components/Router";
@@ -6,32 +5,25 @@ import Router from "../Components/Router";
 const MainContainer = styled.div`
   text-align: center;
   height: 100%;
+  width: 100%;
 `;
 
 const RouterContainer = styled.div`
   position: absolute;
   left: 280px;
   width: 80%;
+  max-width: 100%;
+  min-width: 900px;
   margin: 20px;
   white-space: normal;
   align-content: center;
   min-height: 400px;
-  max-height: 
+  height: 95%;
 `;
 
 function App() {
   return (
     <MainContainer>
-      <Navigator
-        PaperProps={{
-          style: {
-            height: 100 + "%",
-            backgroundColor: "#f6f6f6",
-            padding: "0 20px",
-            display: "inline-block",
-          },
-        }}
-      />
       <RouterContainer>
         <Router />
       </RouterContainer>
