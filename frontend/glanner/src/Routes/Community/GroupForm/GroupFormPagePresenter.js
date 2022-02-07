09cf2738456f55c935712a531127cd1313501c12
JYA <abogado2113@gmail.com>
2022-02-07 18:28:24 +0900

    S06P12A606-47 feat: 그룹 모집 게시판 작성페이지에서 태그 추가 기능
    downshift 라이브러리 이용하여 태그 추가 기능 추가
    태그당 6글자, 최대 5개까지 추가 가능

---
+++ frontend/glanner/src/Routes/Community/GroupForm/GroupFormPagePresenter.js
@@ -0,0 +1,312 @@
+import { DeleteForever } from "@material-ui/icons";
+import { Box, Button, Divider, TextField, Grid, Typography, CardMedia, ImageListItem, ImageListItemBar, IconButton, Autocomplete, Stack, Chip } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { maxWidth } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
+import { boardStyles } from "../Board.styles";
+import TagsInput from "./CustomHook";
+
+const useStyles = makeStyles(boardStyles);
+
+export const GroupFormPagePresenter = ({
+  handle, 
+  handleSubmit, 
+  onFileChange, 
+  deleteFile, 
+  attachment,  
+  data
+}) => {
+  const classes = useStyles();
+  function handleSelecetedTags(items) {
+    console.log(items);
+  }    
+  return (
+      <form noValidate onSubmit={(e) => e.preventDefault()} style={{ padding: '40px 60px 40px 80px'}}>            
+        <Grid container>          
+          <Grid item xs={1.2}>
+            <Typography className={classes.groupLabel}>제목</Typography>
+          </Grid>
+          <Grid item xs={10}>            
+            <TextField
+              onChange={(e) => {
+                // if (data.title) {setTitleError(false)}
+                handle(e)
+              }}
+              value={data.title}              
+              // label="제목"
+              id="title" 
+              fullWidth
+              required
+              // error={titleError}
+              className={[classes.smallInput, classes.field].join(' ')}
+              sx={{flexGrow: 2}}
+            >            
+            </TextField>
+          </Grid>
+        </Grid>
+
+        <Grid container>          
+          <Grid item xs={1.2}>
+            <Typography className={classes.groupLabel}>인원수</Typography>
+          </Grid>
+          <Grid item xs={1.5}>            
+            <TextField
+              select
+              onChange={(e) => {
+                // if (data.title) {setTitleError(false)}
+                handle(e)
+              }}
+              value={data.title}              
+              // label="제목"
+              id="title" 
+              fullWidth
+              required
+              // error={titleError}
+              className={[classes.smallInput, classes.field].join(' ')}
+              sx={{flexGrow: 2}}
+              InputProps={{maxLength: 4}}
+            >            
+            </TextField>
+          </Grid>
+          <Grid item xs={1.5}>
+            <Typography className={classes.groupLabel}>카테고리</Typography>
+          </Grid>
+          <Grid item xs={7}>
+            <TagsInput
+              className={[classes.smallInput, classes.field].join(' ')}
+              style={{}}
+              selectedTags={handleSelecetedTags}
+              fullWidth
+              variant="outlined"
+              id="tags"
+              name="tags"
+              placeholder="Add Category"
+              // label="tags"
+            />            
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={1.2}>
+            <Typography className={classes.groupLabel}>내용</Typography>
+          </Grid>
+          <Grid item xs={10}>
+            <TextField 
+              onChange={(e) => {
+                // if (data.content) {setContentError(false)}
+                handle(e)
+              }}
+              id="content" 
+              // label="내용"
+              value={data.content}
+              fullWidth
+              required
+              multiline={true}
+              rows={15}
+              // error={contentError}
+              className={classes.field}
+            />
+          </Grid>
+        </Grid>
+        <Grid container sx={{ mt: '20px'}}>
+          <Grid item xs={1.2}>
+            <Typography className={classes.groupLabel}>파일 첨부</Typography>
+          </Grid>
+          <Grid item xs={8.6}>            
+            <TextField value={attachment} className={[classes.smallInput, classes.field].join(' ')} disabled fullWidth />
+          </Grid>
+          <Grid item xs={1}>
+            <TextField
+                style={{ display: 'none' }}
+                id="upload-photo"
+                name="upload-photo"
+                type="file"
+                onChange={onFileChange}
+                value={data.attachment}                
+              />            
+            <label htmlFor="upload-photo" >  
+              <Box sx={{ mt: -0.2, 
+                    ml: 1.3 }}>                                 
+                <Button component="span" className={[classes.attBtn, classes.attBtnText].join(' ')}>                  
+                  첨부하기
+                </Button>
+              </Box>              
+            </label>            
+          </Grid>
+          <Grid item xs={1} />
+
+          {/* 첨부한 이미지 미리보기 */}
+          {attachment && 
+            <>
+              <ImageListItem sx={{mt: 2, ml: 14}}>
+                <CardMedia
+                component="img"
+                sx={{ width: 100, height: 100 }}
+                image={attachment}
+                alt="Live from space album cover"
+              />
+              <ImageListItemBar              
+                  sx={{
+                    background:
+                      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',                    
+                  }}
+                  position="top"
+                  actionIcon={
+                    <IconButton
+                      onClick={deleteFile}
+                      sx={{ color: 'white' }}                  
+                    >
+                      <DeleteForever />
+                    </IconButton>
+                  }
+                  actionPosition="right"
+                />                
+              </ImageListItem>              
+            </>            
+          }
+        </Grid>
+        <Divider sx={{ mt: 2}} />
+        <Grid container sx={{mt: 2, justifyContent: 'space-between'}}>
+          {/* <Grid item xs={2}/> */}
+          <Grid item>
+            <Link to={'/community'}>
+              <Button className={classes.btn} >목록으로</Button>
+            </Link>
+          </Grid>
+          <Grid item sx={{ ml: 1}}>
+            <Button className={classes.btn} type="submit" >작성하기</Button>
+          </Grid>
+        </Grid>
+      </form>
+  )
+};
+
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+  },
+  { title: 'The Good, the Bad and the Ugly', year: 1966 },
+  { title: 'Fight Club', year: 1999 },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: 2001,
+  },
+  {
+    title: 'Star Wars: Episode V - The Empire Strikes Back',
+    year: 1980,
+  },
+  { title: 'Forrest Gump', year: 1994 },
+  { title: 'Inception', year: 2010 },
+  {
+    title: 'The Lord of the Rings: The Two Towers',
+    year: 2002,
+  },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: 'Goodfellas', year: 1990 },
+  { title: 'The Matrix', year: 1999 },
+  { title: 'Seven Samurai', year: 1954 },
+  {
+    title: 'Star Wars: Episode IV - A New Hope',
+    year: 1977,
+  },
+  { title: 'City of God', year: 2002 },
+  { title: 'Se7en', year: 1995 },
+  { title: 'The Silence of the Lambs', year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: 'Life Is Beautiful', year: 1997 },
+  { title: 'The Usual Suspects', year: 1995 },
+  { title: 'Léon: The Professional', year: 1994 },
+  { title: 'Spirited Away', year: 2001 },
+  { title: 'Saving Private Ryan', year: 1998 },
+  { title: 'Once Upon a Time in the West', year: 1968 },
+  { title: 'American History X', year: 1998 },
+  { title: 'Interstellar', year: 2014 },
+  { title: 'Casablanca', year: 1942 },
+  { title: 'City Lights', year: 1931 },
+  { title: 'Psycho', year: 1960 },
+  { title: 'The Green Mile', year: 1999 },
+  { title: 'The Intouchables', year: 2011 },
+  { title: 'Modern Times', year: 1936 },
+  { title: 'Raiders of the Lost Ark', year: 1981 },
+  { title: 'Rear Window', year: 1954 },
+  { title: 'The Pianist', year: 2002 },
+  { title: 'The Departed', year: 2006 },
+  { title: 'Terminator 2: Judgment Day', year: 1991 },
+  { title: 'Back to the Future', year: 1985 },
+  { title: 'Whiplash', year: 2014 },
+  { title: 'Gladiator', year: 2000 },
+  { title: 'Memento', year: 2000 },
+  { title: 'The Prestige', year: 2006 },
+  { title: 'The Lion King', year: 1994 },
+  { title: 'Apocalypse Now', year: 1979 },
+  { title: 'Alien', year: 1979 },
+  { title: 'Sunset Boulevard', year: 1950 },
+  {
+    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+    year: 1964,
+  },
+  { title: 'The Great Dictator', year: 1940 },
+  { title: 'Cinema Paradiso', year: 1988 },
+  { title: 'The Lives of Others', year: 2006 },
+  { title: 'Grave of the Fireflies', year: 1988 },
+  { title: 'Paths of Glory', year: 1957 },
+  { title: 'Django Unchained', year: 2012 },
+  { title: 'The Shining', year: 1980 },
+  { title: 'WALL·E', year: 2008 },
+  { title: 'American Beauty', year: 1999 },
+  { title: 'The Dark Knight Rises', year: 2012 },
+  { title: 'Princess Mononoke', year: 1997 },
+  { title: 'Aliens', year: 1986 },
+  { title: 'Oldboy', year: 2003 },
+  { title: 'Once Upon a Time in America', year: 1984 },
+  { title: 'Witness for the Prosecution', year: 1957 },
+  { title: 'Das Boot', year: 1981 },
+  { title: 'Citizen Kane', year: 1941 },
+  { title: 'North by Northwest', year: 1959 },
+  { title: 'Vertigo', year: 1958 },
+  {
+    title: 'Star Wars: Episode VI - Return of the Jedi',
+    year: 1983,
+  },
+  { title: 'Reservoir Dogs', year: 1992 },
+  { title: 'Braveheart', year: 1995 },
+  { title: 'M', year: 1931 },
+  { title: 'Requiem for a Dream', year: 2000 },
+  { title: 'Amélie', year: 2001 },
+  { title: 'A Clockwork Orange', year: 1971 },
+  { title: 'Like Stars on Earth', year: 2007 },
+  { title: 'Taxi Driver', year: 1976 },
+  { title: 'Lawrence of Arabia', year: 1962 },
+  { title: 'Double Indemnity', year: 1944 },
+  {
+    title: 'Eternal Sunshine of the Spotless Mind',
+    year: 2004,
+  },
+  { title: 'Amadeus', year: 1984 },
+  { title: 'To Kill a Mockingbird', year: 1962 },
+  { title: 'Toy Story 3', year: 2010 },
+  { title: 'Logan', year: 2017 },
+  { title: 'Full Metal Jacket', year: 1987 },
+  { title: 'Dangal', year: 2016 },
+  { title: 'The Sting', year: 1973 },
+  { title: '2001: A Space Odyssey', year: 1968 },
+  { title: "Singin' in the Rain", year: 1952 },
+  { title: 'Toy Story', year: 1995 },
+  { title: 'Bicycle Thieves', year: 1948 },
+  { title: 'The Kid', year: 1921 },
+  { title: 'Inglourious Basterds', year: 2009 },
+  { title: 'Snatch', year: 2000 },
+  { title: '3 Idiots', year: 2009 },
+  { title: 'Monty Python and the Holy Grail', year: 1975 },
+];
