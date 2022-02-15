0e6f32a8e350b37aceff7c92616aaee3e9bc66f7
정주헌 <cherish8513@naver.com>
2022-02-15 13:47:52 +0900

    v1.3.4 release

--- backend/glanner/src/main/java/com/glanner/api/exception/MailNotSentException.java
+++
@@ -1,5 +0,0 @@
-package com.glanner.api.exception;
-
-public class MailNotSentException extends RuntimeException{
-    public MailNotSentException() {}
-}
--- backend/glanner/src/main/java/com/glanner/core/repository/FreeBoardCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glanner.core.repository;
-
-import com.glanner.core.domain.board.FreeBoard;
-
-import java.util.Optional;
-
-public interface FreeBoardCustomRepository {
-    Optional<FreeBoard> findRealById(Long id);
-}
