a862ec304ad1f46d36a24329953c128dacc2a2e2
JYA <ahndoc@naver.com>
2022-02-17 11:03:35 +0900

    delete backend

--- backend/glanner/src/main/java/com/glanner/core/domain/user/UserRoleStatus.java
+++
@@ -1,5 +0,0 @@
-package com.glanner.core.domain.user;
-
-public enum UserRoleStatus {
-    ROLE_USER, ROLE_ADMIN
-}
--- backend/glanner/src/main/java/com/glanner/core/repository/ConferenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.glanner.core.repository;
-
-import com.glanner.core.domain.user.Conference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConferenceRepository extends JpaRepository<Conference, Long> {
-}
