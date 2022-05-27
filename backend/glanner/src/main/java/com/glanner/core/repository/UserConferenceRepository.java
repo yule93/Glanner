c928a01357886af3558c4a1cd64ec57cfea07645
MJS <dbfm2222@gmail.com>
2022-05-27 17:49:45 +0900

    🗑️ chore: 재백업을 위해 파일 삭제

--- backend/glanner/src/main/java/com/glanner/core/repository/UserConferenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.glanner.core.repository;
-
-import com.glanner.core.domain.user.UserConference;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserConferenceRepository extends JpaRepository<UserConference, Long> {
-}
